a2aa01a4b8f1cafa27dc9c9cfe85b1e5af534b12
Torres-Neto <torresneto.tn@gmail.com>
2022-09-23 22:54:46 -0300

    exercicios

--- Python/PythonExercicios/ex005.py
+++ Python/PythonExercicios/ex005.py
@@ -1,8 +1,11 @@
 from cmath import sqrt
 
-
+'''
+    n = int(input('Digite um número qualquer: '))
+    a = n - 1
+    s = n + 1
+    print('O Antecessor do número {}, é: {}!'.format(n, a))
+    print('E o seu Sucessor é {}!'.format(s))
+'''
 n = int(input('Digite um número qualquer: '))
-a = n - 1
-s = n + 1
-print('O Antecessor do número {}, é: {}!'.format(n, a))
-print('E o seu Sucessor é {}!'.format(s))
+print('O Antecessor do número {}, é: {} e o seu Sucessor é: {}'.format(n, n-1, n+1))
--- Python/PythonExercicios/ex008.py
+++ Python/PythonExercicios/ex008.py
@@ -1,6 +1,17 @@
 # Ler um valor em metros e o exibir convertido em centímetros e milímetros.
 
-m = float(input('Digite um valor em Metros(m): '))
+m = int(input('Uma distância em Metros(m): '))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
 cm = m * 100
 mm = m * 1000
-print('{}m são: {}cm e {}mm'.format(m, cm, mm))
+print('A medida de {}m corresponde a:'.format(float(m)))
+print('{}km'.format(km))
+print('{}hm'.format(hm))
+print('{}dam'.format(dam))
+print('{}dm'.format(dm))
+print('{}cm'.format(cm))
+print('{}mm'.format(mm))
+print('')
